c1068ecbeef91227c4a5e4c709490d0e559cc80c
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-25 10:03:23 -0300

    Prettier

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Router } from 'express'
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
-const transactionRouter = Router();
+const transactionRouter = Router()
 
 // const transactionsRepository = new TransactionsRepository();
 
@@ -11,16 +11,16 @@ transactionRouter.get('/', (request, response) => {
   try {
     // TODO
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ error: err.message })
   }
-});
+})
 
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({ error: err.message })
   }
-});
+})
 
-export default transactionRouter;
+export default transactionRouter
