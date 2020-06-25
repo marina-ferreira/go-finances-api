93dc8a2536028710b1709659c892e28759259f6c
Marina Ferreira <marina.ferreira.dev@gmail.com>
2020-06-25 11:48:34 -0300

    Balance validation

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,14 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance()
+    const isBroken = type === 'outcome' && balance.total - value < 0
+
+    if (isBroken) {
+      const moessage = `You do not have enough balance. Current balance: $${balance.total}`
+      throw Error(moessage)
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
